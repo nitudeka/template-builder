0149ddae744e65e88275bb544a60e8f2d6cd7a62
nitudeka <iamnitudeka@gmail.com>
2025-01-22 10:58:27 +0530

    improved UI

--- template-builder-frontend/src/components/Builder/Nodes/TicketField.jsx
+++ template-builder-frontend/src/components/Builder/Nodes/TicketField.jsx
@@ -5,7 +5,7 @@ const TicketFieldNode = ({ setSelectedField, nodeData, parentComponentData }) =>
 
   return (
     <div onClick={onClick} className='h-full min-h-8 pr-1 pt-1'>
-      <div className='hover:bg-gray-200 rounded-sm hover:border border-solid h-full flex flex-col text-2xs cursor-pointer'>
+      <div className={[nodeData.title ? '' : 'ticket-field', 'hover:bg-gray-200 hover:bg-gray-200 rounded-sm h-full flex flex-col text-2xs cursor-pointer'].join(' ')}>
 	<span className='whitespace-nowrap text-2xs text-gray-600'>
 	{nodeData.title}
 	</span>

464d5a60e1c3a48d8f7f806efb4c3f74fd398d1d
nitudeka <iamnitudeka@gmail.com>
2025-01-22 10:50:29 +0530

    fixed height issue

--- template-builder-frontend/src/components/Builder/Nodes/TicketField.jsx
+++ template-builder-frontend/src/components/Builder/Nodes/TicketField.jsx
@@ -4,8 +4,8 @@ const TicketFieldNode = ({ setSelectedField, nodeData, parentComponentData }) =>
   }
 
   return (
-    <div onClick={onClick} className='h-full pr-1 pt-1'>
-      <div className='hover:bg-gray-200 rounded-sm h-full flex flex-col text-2xs cursor-pointer'>
+    <div onClick={onClick} className='h-full min-h-8 pr-1 pt-1'>
+      <div className='hover:bg-gray-200 rounded-sm hover:border border-solid h-full flex flex-col text-2xs cursor-pointer'>
 	<span className='whitespace-nowrap text-2xs text-gray-600'>
 	{nodeData.title}
 	</span>
